63fa0b8a70b57cafa6ee23d062a421d8c08f7a1b
Deavoe <123446640+Deavoe@users.noreply.github.com>
2023-02-13 20:29:08 -0500

    Prepare appearance of submission form and book

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// Toggle form visibility
+const form = document.getElementById('signup');
+function toggleForm() {
+  form.classList.toggle('hidden');
+}
+document.querySelector('.cancel-form').addEventListener('click', toggleForm);
+document.querySelector('.add').addEventListener('click', toggleForm);
+
 let myLibrary = [];
 
 function Book() {
